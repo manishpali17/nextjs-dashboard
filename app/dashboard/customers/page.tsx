201c036ed6301964bced187f7ffde5e656d166e4
manish-pali <129107899+manish-pali@users.noreply.github.com>
2024-02-12 11:13:37 +0530

    added custmer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function Page(){
-    return <p>customers page</p>
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+      <Table customers={customers}></Table>
+    </Suspense>
+  );
 }
