bb23e70eb5b7c143c815ddd58875ef599d45349f
manish-pali <129107899+manish-pali@users.noreply.github.com>
2024-02-12 12:08:29 +0530

    no catch to fetchFilteredCustomers

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -193,6 +193,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore();
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
